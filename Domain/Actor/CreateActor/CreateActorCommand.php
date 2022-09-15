a773024a21c7a827e927db668db77b8eb921cf79
Florent <florent.sorel@gmail.com>
2022-09-15 11:29:18 +0200

    refactor: remove dead code

--- Domain/Actor/CreateActor/CreateActorCommand.php
+++ Domain/Actor/CreateActor/CreateActorCommand.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Domain\Actor\CreateActor;
 
 use Domain\Shared\Bus\Command\Command;
-use Symfony\Component\HttpFoundation\InputBag;
 
 final class CreateActorCommand implements Command
 {
@@ -13,14 +12,6 @@ final class CreateActorCommand implements Command
         private readonly string $name,
         private readonly bool $active
     ) {
-    }
-
-    public static function fromRequest(InputBag $request): self
-    {
-        return new self(
-            $request->get('name'),
-            1 === (int) $request->get('active'),
-        );
     }
 
     public function getName(): string
